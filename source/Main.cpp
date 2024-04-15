c7e5b2e04ad65269bc2d31ac225120bdc31eadf2
Robott11 <contato.robott@gmail.com>
2024-04-15 02:29:22 -0300

    first function hook with pluginsdk

---
+++ source/Main.cpp
@@ -0,0 +1,90 @@
+#include "plugin.h"
+#include "Events.h"
+#include "CPlaneTrails.h"
+
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace plugin;
+
+
+class Test {
+public:
+    Test() {
+        openConsole();
+
+        static StdcallEvent <AddressList<0x4E9EF3, H_CALL>, PRIORITY_BEFORE, ArgPickN<signed char, 0>, void(signed char)> onGetRadioStationName;
+        onGetRadioStationName += [](signed char station) {
+            switch (station)
+            {
+                case eRadioID::RADIO_K_DST:
+                    std::cout << "RADIO_K_DST" << std::endl;
+                    break;
+
+                case eRadioID::RADIO_PLAYBACK_FM:
+                    std::cout << "RADIO_PLAYBACK_FM" << std::endl;
+                    break;
+
+                case eRadioID::RADIO_K_ROSE:
+                    std::cout << "RADIO_K_ROSE" << std::endl;
+                    break;
+
+                case eRadioID::RADIO_BOUNCE_FM:
+                    std::cout << "RADIO_BOUNCE_FM" << std::endl;
+                    break;
+
+                case eRadioID::RADIO_SF_UR:
+                    std::cout << "RADIO_SF_UR" << std::endl;
+                    break;
+
+                case eRadioID::RADIO_LOS_SANTOS:
+                    std::cout << "RADIO_LOS_SANTOS" << std::endl;
+                    break;
+
+                case eRadioID::RADIO_RADIO_X:
+                    std::cout << "RADIO_RADIO_X" << std::endl;
+                    break;
+
+                case eRadioID::RADIO_CSR_103_9:
+                    std::cout << "RADIO_CSR_103_9" << std::endl;
+                    break;
+
+                case eRadioID::RADIO_K_JAH_WEST:
+                    std::cout << "RADIO_K_JAH_WEST" << std::endl;
+                    break;
+
+                case eRadioID::RADIO_MASTER_SOUNDS_98_3:
+                    std::cout << "RADIO_MASTER_SOUNDS_98_3" << std::endl;
+                    break;
+
+                case eRadioID::RADIO_WCTR:
+                    std::cout << "RADIO_WCTR" << std::endl;
+                    break;
+
+                case eRadioID::RADIO_USER_TRACKS:
+                    std::cout << "RADIO_USER_TRACKS" << std::endl;
+                    break;
+
+                case eRadioID::RADIO_NONE:
+                    std::cout << "RADIO_NONE" << std::endl;
+                    break;
+
+                default:
+                    break;
+            }
+        };
+
+    }
+        
+
+private:
+
+    void openConsole() {
+        AllocConsole();
+        FILE* fs{};
+        freopen_s(&fs, "CONIN$", "r", stdin);
+        freopen_s(&fs, "CONOUT$", "w", stdout);
+        freopen_s(&fs, "CONOUT$", "w", stderr);
+    }
+} TestPlugin;
