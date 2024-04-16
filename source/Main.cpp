acaa1928097f95a2f267f047ed463884a8af9990
Robott11 <contato.robott@gmail.com>
2024-04-15 23:41:21 -0300

    feat: add my first redirect function hook

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,79 +1,102 @@
 #include "plugin.h"
 #include "Events.h"
-#include "CPlaneTrails.h"
+#include "CSprite.h"
+#include "CFont.h"
 
 
 #include <iostream>
 #include <stdio.h>
+#include <string>
+
+#define MAX_PATH 260
+
+int SetSaveDirectoryHook(char* name)
+{
+    (void)name;
+
+    std::cout << "HELLO WORLD FROM HookTest()" << std::endl;
+    const char* NewDir = "C:\\Users\\ROBOTT\\Downloads\\test";
+
+    sprintf_s(*(char(*)[MAX_PATH])0xC16F18, "%s\\%s", NewDir, "GTASAsf");
+
+    return 0;
+}
 
 using namespace plugin;
 
 
 class Test {
 public:
-    Test() {
-        openConsole();
+    Test()
+    {
+        OpenConsole();
+        HookInstallEvents();
+    }
+        
+private:
 
+    void HookInstallEvents() const
+    {
         static StdcallEvent <AddressList<0x4E9EF3, H_CALL>, PRIORITY_BEFORE, ArgPickN<signed char, 0>, void(signed char)> onGetRadioStationName;
         onGetRadioStationName += [](signed char station) {
             switch (station)
             {
-                case eRadioID::RADIO_K_DST:
-                    std::cout << "RADIO_K_DST" << std::endl;
-                    break;
+            case eRadioID::RADIO_K_DST:
+                std::cout << "RADIO_K_DST" << std::endl;
+                break;
 
-                case eRadioID::RADIO_PLAYBACK_FM:
-                    std::cout << "RADIO_PLAYBACK_FM" << std::endl;
-                    break;
+            case eRadioID::RADIO_PLAYBACK_FM:
+                std::cout << "RADIO_PLAYBACK_FM" << std::endl;
+                break;
 
-                case eRadioID::RADIO_K_ROSE:
-                    std::cout << "RADIO_K_ROSE" << std::endl;
-                    break;
+            case eRadioID::RADIO_K_ROSE:
+                std::cout << "RADIO_K_ROSE" << std::endl;
+                break;
 
-                case eRadioID::RADIO_BOUNCE_FM:
-                    std::cout << "RADIO_BOUNCE_FM" << std::endl;
-                    break;
+            case eRadioID::RADIO_BOUNCE_FM:
+                std::cout << "RADIO_BOUNCE_FM" << std::endl;
+                break;
 
-                case eRadioID::RADIO_SF_UR:
-                    std::cout << "RADIO_SF_UR" << std::endl;
-                    break;
+            case eRadioID::RADIO_SF_UR:
+                std::cout << "RADIO_SF_UR" << std::endl;
+                break;
 
-                case eRadioID::RADIO_LOS_SANTOS:
-                    std::cout << "RADIO_LOS_SANTOS" << std::endl;
-                    break;
+            case eRadioID::RADIO_LOS_SANTOS:
+                std::cout << "RADIO_LOS_SANTOS" << std::endl;
+                break;
 
-                case eRadioID::RADIO_RADIO_X:
-                    std::cout << "RADIO_RADIO_X" << std::endl;
-                    break;
+            case eRadioID::RADIO_RADIO_X:
+                std::cout << "RADIO_RADIO_X" << std::endl;
+                break;
 
-                case eRadioID::RADIO_CSR_103_9:
-                    std::cout << "RADIO_CSR_103_9" << std::endl;
-                    break;
+            case eRadioID::RADIO_CSR_103_9:
+                std::cout << "RADIO_CSR_103_9" << std::endl;
+                break;
 
-                case eRadioID::RADIO_K_JAH_WEST:
-                    std::cout << "RADIO_K_JAH_WEST" << std::endl;
-                    break;
+            case eRadioID::RADIO_K_JAH_WEST:
+                std::cout << "RADIO_K_JAH_WEST" << std::endl;
+                break;
 
-                case eRadioID::RADIO_MASTER_SOUNDS_98_3:
-                    std::cout << "RADIO_MASTER_SOUNDS_98_3" << std::endl;
-                    break;
+            case eRadioID::RADIO_MASTER_SOUNDS_98_3:
+                std::cout << "RADIO_MASTER_SOUNDS_98_3" << std::endl;
+                break;
 
-                case eRadioID::RADIO_WCTR:
-                    std::cout << "RADIO_WCTR" << std::endl;
-                    break;
+            case eRadioID::RADIO_WCTR:
+                std::cout << "RADIO_WCTR" << std::endl;
+                break;
 
-                case eRadioID::RADIO_USER_TRACKS:
-                    std::cout << "RADIO_USER_TRACKS" << std::endl;
-                    break;
+            case eRadioID::RADIO_USER_TRACKS:
+                std::cout << "RADIO_USER_TRACKS" << std::endl;
+                break;
 
-                case eRadioID::RADIO_NONE:
-                    std::cout << "RADIO_NONE" << std::endl;
-                    break;
+            case eRadioID::RADIO_NONE:
+                std::cout << "RADIO_NONE" << std::endl;
+                break;
 
-                default:
-                    break;
+            default:
+                break;
             }
-        };
+            };
 
 
         static ThiscallEvent <
@@ -83,15 +106,34 @@ public:
             void(CPathFind*, int, const CVector*, const CVector*, CNodeAddress*, CNodeAddress**)
         > onComputePathFind;
 
-        onComputePathFind += [](CPathFind* pathFind, int nodeType, const CVector* ref_origin, const CVector* ref_dest, CNodeAddress* ref_firstNode, CNodeAddress** ref_route) {
+        onComputePathFind += [&](CPathFind* pathFind, int nodeType, const CVector* ref_origin, const CVector* ref_dest, CNodeAddress* ref_firstNode, CNodeAddress** ref_route) mutable {
             std::cout << "onComputePathFind" << std::endl;
         };
+
+        patch::RedirectCall({ 0x747476 }, SetSaveDirectoryHook);
     }
-        
 
-private:
+    void PrintText2ScreenFromWorldPos(const std::string text, const CVector& posn, CRGBA color) const
+    {
+        RwV3d rwp = { posn.x, posn.y, posn.z + 1.0f };
+        RwV3d screenCoors; 
+        float w, h;
 
-    void openConsole() {
+        if (CSprite::CalcScreenCoors(rwp, &screenCoors, &w, &h, true, true)) {
+            CFont::SetOrientation(ALIGN_CENTER);
+            CFont::SetColor(color);
+            CFont::SetDropShadowPosition(1);
+            CFont::SetBackground(false, false);
+            CFont::SetWrapx(500.0);
+            CFont::SetScale(0.5, 1.0);
+            CFont::SetFontStyle(FONT_SUBTITLES);
+            CFont::SetProportional(true);
+            CFont::PrintString(screenCoors.x, screenCoors.y, text.c_str());
+        }
+    }
+
+    void OpenConsole() const
+    {
         AllocConsole();
         FILE* fs{};
         freopen_s(&fs, "CONIN$", "r", stdin);
